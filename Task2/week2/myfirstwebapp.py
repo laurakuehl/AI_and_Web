062d17c1da6df2ef0dbef6f5c9a9e4db07031dca
kolutter <konstantin.lutter@gmail.com>
2025-01-06 14:21:47 +0100

    autokorrektur hinzugefügt, der crawler müllt jz bei mehrfacher anwendung nicht mehr den index zu sondern checkt vorher obs das schon gibt, requirements ergänzt

--- Task2/week2/myfirstwebapp.py
+++ Task2/week2/myfirstwebapp.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
+from textblob import TextBlob
+from whoosh.query import Term
 
 app = Flask(__name__)
 
@@ -28,7 +30,16 @@ def search():
                 {"title": r["title"], "url": r["url"]}
                 for r in search_results
             ]
-
+            #if no result is found, use autocorrecter and search again for right spelling
+            if not results:
+                blob = TextBlob(q.text)
+                corrected_query = blob.correct()
+                print(f"No result for {q.text}, searching for {corrected_query}")
+                search_results = searcher.search(Term("content", corrected_query.string))
+                results = [
+                    {"title": r["title"], "url": r["url"]}
+                    for r in search_results
+                ]
     return render_template("search.html", query=query, results=results)
 
 
